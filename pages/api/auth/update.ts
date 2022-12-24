04f9751640fa4c554f40ccbdfe00fdd8e8b967f6
Amin Charoliya <amincharoliya@gmail.com>
2022-12-24 21:04:24 +0530

    Add update user Endpoint

---
+++ pages/api/auth/update.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import bcryptjs from 'bcryptjs';
+import User from '../../../models/User';
+import db from '../../../utils/db';
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method !== 'POST') {
+		return res.status(400).send({ message: `${req.method} not supported` });
+	}
+
+	const session = await getSession({ req });
+	if (!session) {
+		return res.status(401).send({ message: 'signin required' });
+	}
+
+	const { user } = session;
+	const { name, email, password, image } = req.body;
+	console.log(name);
+
+	if (
+		!name ||
+		!email ||
+		!email.includes('@') ||
+		(password && password.trim().length < 5)
+	) {
+		res.status(422).json({
+			message: 'Validation error',
+		});
+		return;
+	}
+
+	await db.connect();
+	const toUpdateUser = await User.findById(user.id);
+	toUpdateUser.name = name;
+	toUpdateUser.email = email;
+	toUpdateUser.image = image;
+
+	if (password) {
+		toUpdateUser.password = bcryptjs.hashSync(password);
+	}
+
+	await toUpdateUser.save();
+	await db.disconnect();
+	res.send({
+		message: 'User updated',
+	});
+}
+
+export default handler;
