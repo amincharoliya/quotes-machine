850a3d2556861fd7eb81e9deac4cb23255779d5c
Amin Charoliya <amincharoliya@gmail.com>
2022-12-24 20:34:06 +0530

    Set grid column for smaller screens

--- components/members/member.tsx
+++ components/members/member.tsx
@@ -5,7 +5,7 @@ export default function Member({ member }) {
 	const date = new Date(member.createdAt).toDateString();
 	return (
 		<div className="bg-white dark:bg-slate-800 p-4">
-			<div className="flex items-center mb-4">
+			<div className="flex items-center">
 				<div className="h-20 w-20 flex items-center justify-center rounded-lg overflow-hidden bg-theme-dark">
 					<Image
 						src={'/images/profiles/' + member.image + '.png'}
