857f2e7e7657a4007c9ca0e0a9e80e93673f4da1
Amin Charoliya <amincharoliya@gmail.com>
2022-12-25 21:51:02 +0530

    Add /api/save endpoint to save new quote

---
+++ pages/api/quotes/save.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import Quote from '../../../models/Quote';
+import db from '../../../utils/db';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	const session = await getSession({ req });
+	if (!session) {
+		return res.status(401).send('Signin required');
+	}
+
+	const { user } = session;
+	await db.connect();
+	const newQuote = new Quote({
+		...req.body,
+		user: user.id,
+		userName: user.name,
+		userImage: user.image,
+	});
+
+	const quote = await newQuote.save();
+	res.status(201).send(quote);
+};
+export default handler;
