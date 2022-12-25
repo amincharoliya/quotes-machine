cfbdbecb2e87e43423ff807849eed994e4dd1871
Amin Charoliya <amincharoliya@gmail.com>
2022-12-25 21:51:27 +0530

    Fix spacing on members page

--- components/members/members.tsx
+++ components/members/members.tsx
@@ -13,7 +13,7 @@ export default function Members() {
 	return (
 		<div>
 			<h1 className="text-3xl text-center font-bold mb-8">Members</h1>
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-12">
 				{!members ? (
 					<>
 						<MembersPlaceholder />
