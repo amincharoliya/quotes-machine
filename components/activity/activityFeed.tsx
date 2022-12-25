0fcaca72c06d04db14c85e7c81dc784430f599cc
Amin Charoliya <amincharoliya@gmail.com>
2022-12-25 21:52:57 +0530

    Fetch new quotes and meanwhile show placeholder

--- components/activity/activityFeed.tsx
+++ components/activity/activityFeed.tsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from 'react';
+import ActivityForm from './activityForm';
+import ActivityPlaceholder from './activityPlaceholder';
 import Activity from './activitySingle';
 
 export default function ActivityFeed() {
+	const [quotes, setQuotes] = useState(null);
+	useEffect(() => {
+		fetch('/api/quotes')
+			.then((data) => data.json())
+			.then((data) => setQuotes(data.reverse()));
+	}, []);
 	return (
 		<>
-			<Activity />
-			<Activity />
-			<Activity />
-			<Activity />
-			<Activity />
-			<Activity />
+			<ActivityForm quotes={quotes} setQuotes={setQuotes} />
+			{quotes ? (
+				quotes.map((quote) => <Activity key={quote._id} data={quote} />)
+			) : (
+				<>
+					<ActivityPlaceholder />
+					<ActivityPlaceholder />
+				</>
+			)}
 		</>
 	);
 }
--- components/activity/activitySingle.tsx
+++ components/activity/activitySingle.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Quote } from '../icons';
 
-export default function Activity() {
+export default function Activity({ data }) {
 	return (
 		<div className="bg-white dark:bg-slate-800 p-4 mb-6 rounded-md max-w-3xl mx-auto">
 			<div className="flex items-center mb-4">
 				<div className="h-20 w-20 flex items-center justify-center rounded-lg overflow-hidden bg-theme-dark">
 					<Image
-						src="/images/profiles/profile-1.png"
+						src={'/images/profiles/' + data.userImage + '.png'}
 						width={64}
 						height={64}
 						alt="Avatar"
@@ -20,22 +20,18 @@ export default function Activity() {
 							href="/"
 							className=" font-bold hover:text-theme-light hover:dark:text-theme-dark duration-300"
 						>
-							Jessica
+							{data.userName}
 						</Link>{' '}
 						posted a new quote
 					</h2>
-					<p>18 sep, 2022</p>
+					<p>{new Date(data.createdAt).toDateString()}</p>
 				</div>
 			</div>
 			<div>
 				<blockquote className="bg-slate-100 dark:bg-slate-900 rounded-md p-4 text-center">
 					<Quote className="text-slate-900 dark:text-white mb-4 h-5 w-5 mx-auto" />
-					<p className="mb-2 text-lg font-notoSans">
-						{
-							"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."
-						}
-					</p>
-					<p className="text-lg font-notoSans">- James Cameron</p>
+					<p className="mb-2 text-lg font-notoSans">{data.quote}</p>
+					<p className="text-lg font-notoSans">- {data.author}</p>
 				</blockquote>
 			</div>
 		</div>
