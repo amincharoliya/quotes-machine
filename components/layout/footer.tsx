bd6b891ce3511280416f31ad60b63171286621fb
Amin Charoliya <amincharoliya@gmail.com>
2022-12-31 15:29:36 +0530

    Update footer link

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
 		<footer className="bg-white dark:bg-slate-900/75 border-t-2 dark:border-slate-50/[0.06] py-4">
 			<div className="text-center lg:container px-5 mx-auto">
 				<Link
-					href="https://github.com/amincharoliya/quotes-machine"
+					href="https://www.amincharoliya.com/"
 					target="_blank"
 					rel="noopener noreferrer"
 					className="hover:text-theme-light hover:dark:text-theme-dark text-lg duration-300"
