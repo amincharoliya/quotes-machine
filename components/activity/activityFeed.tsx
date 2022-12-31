0ba0f239748ca3d14f776600778709b9d2df7878
Amin Charoliya <amincharoliya@gmail.com>
2022-12-31 14:18:19 +0530

    Show notice if quotes are not available

--- components/activity/activityFeed.tsx
+++ components/activity/activityFeed.tsx
@@ -1,19 +1,37 @@
 import { useEffect, useState } from 'react';
+import { Alert } from '../icons';
 import ActivityForm from './activityForm';
 import ActivityPlaceholder from './activityPlaceholder';
 import Activity from './activitySingle';
 
 export default function ActivityFeed() {
 	const [quotes, setQuotes] = useState(null);
+	const [notice, setNotice] = useState(null);
 	useEffect(() => {
 		fetch('/api/quotes')
 			.then((data) => data.json())
-			.then((data) => setQuotes(data.reverse()));
+			.then((data) =>
+				data && data.length
+					? setQuotes(data.reverse())
+					: setNotice('No quotes Found')
+			);
 	}, []);
+
+	if (notice) {
+		return (
+			<>
+				<ActivityForm quotes={quotes} setQuotes={setQuotes} />
+				<p className="flex items-center max-w-3xl mx-auto mb-8 bg-white dark:bg-slate-800 p-4 rounded-md">
+					{' '}
+					<Alert className="w-8 h-8 mr-4" /> {notice}
+				</p>
+			</>
+		);
+	}
 	return (
 		<>
 			<ActivityForm quotes={quotes} setQuotes={setQuotes} />
-			{quotes ? (
+			{quotes && quotes.length ? (
 				quotes.map((quote) => <Activity key={quote._id} data={quote} />)
 			) : (
 				<>
