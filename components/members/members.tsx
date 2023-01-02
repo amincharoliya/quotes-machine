576322bf982febfefedd70016047a2958d84fc1b
Amin Charoliya <amincharoliya@gmail.com>
2023-01-02 08:59:46 +0530

    Fetch members on server

--- components/members/members.tsx
+++ components/members/members.tsx
@@ -1,30 +1,14 @@
-import { useEffect, useState } from 'react';
 import Member from './member';
-import MembersPlaceholder from './membersPlaceholder';
 
-export default function Members() {
-	const [members, setMembers] = useState(null);
-	useEffect(() => {
-		fetch('/api/members')
-			.then((data) => data.json())
-			.then((data) => setMembers(data));
-	}, []);
-
+export default function Members({ data }) {
 	return (
 		<div>
 			<h1 className="text-3xl text-center font-bold mb-8">Members</h1>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-12">
-				{!members ? (
-					<>
-						<MembersPlaceholder />
-						<MembersPlaceholder />
-						<MembersPlaceholder />
-					</>
-				) : (
-					members.users?.map((member) => (
+				{data.users.length &&
+					data.users?.map((member) => (
 						<Member key={member._id} member={member} />
-					))
-				)}
+					))}
 			</div>
 		</div>
 	);
--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -1,14 +1,26 @@
+import { server } from '../../config';
 import Layout from '../../components/layout/index';
 import { defaultMetaProps } from '../../components/layout/meta';
 import Members from '../../components/members/members';
 
-export default function Page() {
+export default function Page({ data }) {
 	const props = { ...defaultMetaProps, title: 'Members | Quote Machine' };
 	return (
 		<Layout meta={props}>
 			<div className="lg:container px-5 mx-auto ">
-				<Members />
+				<Members data={data} />
 			</div>
 		</Layout>
 	);
+}
+
+export async function getServerSideProps() {
+	const res = await fetch(`${server}/api/members/`);
+	const data = await res.json();
+
+	return {
+		props: {
+			data,
+		},
+	};
 }
