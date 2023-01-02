75eeaf6bd500944ad800a49548fcb2968eb853c0
Amin Charoliya <amincharoliya@gmail.com>
2023-01-02 08:55:09 +0530

    Fetch data on server

--- pages/members/[id].tsx
+++ pages/members/[id].tsx
@@ -1,41 +1,15 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import ActivityPlaceholder from '../../components/activity/activityPlaceholder';
+import { server } from '../../config';
 import Activity from '../../components/activity/activitySingle';
 import Layout from '../../components/layout';
 import { defaultMetaProps } from '../../components/layout/meta';
 import Member from '../../components/members/member';
-import MembersPlaceholder from '../../components/members/membersPlaceholder';
 import { Alert } from '../../components/icons';
 
-export default function MemberSingle() {
-	const props = { ...defaultMetaProps, title: 'Members | Quote Machine' };
-	const { query } = useRouter();
-	const [data, setData] = useState(null);
-
-	useEffect(() => {
-		if (query.id) {
-			fetch(`/api/members/${query.id}`)
-				.then((data) => data.json())
-				.then((data) => setData(data));
-		}
-	}, [query]);
-
-	if (!data || !query) {
-		return (
-			<Layout meta={props}>
-				<div className="lg:container px-5 mx-auto ">
-					<div className="max-w-3xl mx-auto mb-8">
-						<MembersPlaceholder />
-					</div>
-					<div>
-						<ActivityPlaceholder />
-						<ActivityPlaceholder />
-					</div>
-				</div>
-			</Layout>
-		);
-	}
+export default function MemberSingle({ data }) {
+	const props = {
+		...defaultMetaProps,
+		title: data.member.name + ' | Quote Machine',
+	};
 
 	return (
 		<Layout meta={props}>
@@ -43,17 +17,31 @@ export default function MemberSingle() {
 				<div className="max-w-3xl mx-auto mb-8">
 					<Member member={data.member} quotes={data.quotes.length} />
 				</div>
-				{data.quotes.reverse().map((quote) => (
-					<Activity key={quote._id} data={quote} />
-				))}
 				{data.quotes.length === 0 ? (
 					<p className="flex items-center max-w-3xl mx-auto mb-8 bg-white dark:bg-slate-800 p-4 rounded-md">
 						<Alert className="w-8 h-8 mr-4" /> No quotes found
 					</p>
 				) : (
-					''
+					data.quotes
+						.reverse()
+						.map((quote) => (
+							<Activity key={quote._id} data={quote} />
+						))
 				)}
 			</div>
 		</Layout>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const id = context.params.id;
+
+	const res = await fetch(`${server}/api/members/${id}`);
+	const data = await res.json();
+
+	return {
+		props: {
+			data,
+		},
+	};
 }
