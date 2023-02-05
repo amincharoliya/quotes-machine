3846b0dd1032f1d720311763612b9829569b61b8
Amin Charoliya <amincharoliya@gmail.com>
2023-02-05 18:37:25 +0530

    Add ability in activity feed to load more quotes

--- components/activity/activityFeed.tsx
+++ components/activity/activityFeed.tsx
@@ -1,5 +1,6 @@
+import clsx from 'clsx';
 import { useEffect, useRef, useState } from 'react';
-import { Alert } from '../icons';
+import { Alert, ArrowRound } from '../icons';
 import ActivityForm from './activityForm';
 import ActivityPlaceholder from './activityPlaceholder';
 import Activity from './activitySingle';
@@ -7,6 +8,9 @@ import Activity from './activitySingle';
 export default function ActivityFeed() {
 	const [quotes, setQuotes] = useState(null);
 	const [notice, setNotice] = useState(null);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [pages, setPages] = useState(1);
+	const [loadingMore, setLoadingMore] = useState(false);
 	const intervalId = useRef(null);
 
 	useEffect(() => {
@@ -19,9 +23,12 @@ export default function ActivityFeed() {
 				.then((data) => data.json())
 				.then((data) => {
 					clearInterval(intervalId.current);
-					data && data.length
-						? setQuotes(data.reverse())
+					data && data.quotes.length
+						? setQuotes(data.quotes.reverse())
 						: setNotice('No quotes Found');
+					data && data.totalPages > 1
+						? setPages(data.totalPages)
+						: setPages(1);
 				});
 		}
 
@@ -29,6 +36,18 @@ export default function ActivityFeed() {
 
 		return () => clearInterval(intervalId.current);
 	}, []);
+
+	const loadMoreQuotes = () => {
+		setLoadingMore(true);
+		fetch(`/api/quotes?page=${currentPage + 1}`)
+			.then((data) => data.json())
+			.then((data) => {
+				const updatedQuotes = [...quotes, ...data.quotes.reverse()];
+				setQuotes(updatedQuotes);
+				setCurrentPage(currentPage + 1);
+				setLoadingMore(false);
+			});
+	};
 
 	if (notice) {
 		return (
@@ -59,6 +78,25 @@ export default function ActivityFeed() {
 					<ActivityPlaceholder />
 					<ActivityPlaceholder />
 				</>
+			)}
+
+			{pages > 1 && currentPage < pages ? (
+				<button
+					className={clsx(
+						'px-4 py-2 bg-theme-light text-white rounded-md flex items-center mx-auto my-5',
+						loadingMore && 'pointer-events-none opacity-80'
+					)}
+					onClick={() => loadMoreQuotes()}
+				>
+					{loadingMore ? 'Loading More' : 'Load More'}
+					{loadingMore ? (
+						<ArrowRound className="h-4 w-4 ml-2 animate-spin" />
+					) : (
+						''
+					)}
+				</button>
+			) : (
+				''
 			)}
 		</>
 	);
